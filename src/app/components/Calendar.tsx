8712d15d038a7ef4342d94ec5c4ad05acc63cc10
Diego Huaccha Diestra <dhuacchadiestra@gmail.com>
2024-04-17 22:22:55 -0400

    feat: :sparkles: Add responsive calendar and styles

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,17 +1,48 @@
-"use client"
+'use client'
 
-import React from "react";
-import FullCalendar from "@fullcalendar/react";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import esLocale from "@fullcalendar/core/locales/es";
+import React, { useEffect, useState } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import esLocale from '@fullcalendar/core/locales/es'
 
 export default function Calendar() {
+  const [viewDays, setViewDays] = useState(3)
+
+  const handleWindowResize = () => {
+    if (window.outerWidth > 1024) {
+      setViewDays(7)
+    } else if (window.outerWidth > 768) {
+      setViewDays(5)
+    } else if (window.outerWidth > 600) {
+      setViewDays(4)
+    } else {
+      setViewDays(3)
+    }
+  }
+
+  useEffect(() => {
+    handleWindowResize()
+    window.addEventListener('resize', handleWindowResize)
+  }, [])
+
   return (
     <FullCalendar
       plugins={[timeGridPlugin]}
+      initialView={'timeGridCustomDays'}
+      headerToolbar={{
+        left: 'title',
+        right: 'prev,next',
+      }}
       locale={esLocale}
-      initialView="timeGridWeek"
-      eventSources={[{ url: "/api/org/a/events" }]}
+      nowIndicator={true}
+      eventSources={[{ url: '' }]}
+      views={{
+        timeGridCustomDays: {
+          type: 'timeGrid',
+          duration: { days: viewDays },
+        },
+      }}
+      titleFormat={{ year: 'numeric', month: 'long' }}
     />
-  );
-};
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,73 @@
-import React from "react";
-import Calendar from "./components/Calendar";
+'use client'
+
+import React from 'react'
+import Calendar from './components/Calendar'
 
 export default function Home() {
+  const handleMenu = () => {
+    const sidebar = document.querySelector('#sidebar')
+    sidebar?.classList.toggle('show')
+  }
+
   return (
-    <main className="min-h-[100vh] flex items-center justify-center">
-      <div>
+    <main className="min-h-[100vh] flex flex-col items-center justify-center">
+      <div id="sidebar" className="sidebar bg-white p-6 flex flex-col gap-4">
+        <div onClick={handleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-zinc-600"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M5 12l14 0" />
+            <path d="M5 12l6 6" />
+            <path d="M5 12l6 -6" />
+          </svg>
+        </div>
         <div>
-          <h1 className="text-xl">📆 Calendari.os UC</h1>
+          <h1 className="text-xl">📆 Calendari.os</h1>
+        </div>
+        <div>
+          <p>Minimap view</p>
+        </div>
+        <div>
+          <p>Filtros</p>
         </div>
       </div>
-      <section className="w-full h-full max-h-full">
+      <div className="relative w-full flex justify-center items-center gap-4 px-2 py-2 border-b border-zinc-300 mb-6">
+        <div className="absolute left-4" onClick={handleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-zinc-600"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 6l16 0" />
+            <path d="M4 12l16 0" />
+            <path d="M4 18l16 0" />
+          </svg>
+        </div>
+        <p className="text-[40px]">📆</p>
+        <div>
+          <h1 className="text-xl text-left text-zinc-950">Calendari.os</h1>
+          <p className="text-sm text-left text-zinc-500">By OSUC</p>
+        </div>
+      </div>
+      <section className="w-full h-full">
         <Calendar />
       </section>
     </main>
